6803b056e08a2b514addd90e14796d5106ddfd65
Monica Vu <vu.monica.05@gmail.com>
2020-08-22 16:30:35 -0400

    added header folder

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ import RSSFeed from "../components/RSSFeed/RSSFeed";
 import Calendar from "../components/Calendar/Calendar"
 import AboutTechnolgapSection from "../components/AboutTechnolgapSection/AboutTechnolgapSection";
 import ContactSection from "../components/ContactSection/ContactSection";
+import Header from "../components/Header/Header";
 
 const Index = () => <Layout>
     This is the context for the home page.
+    <Header /> 
 
     <AboutTechnolgapSection /> 
     
