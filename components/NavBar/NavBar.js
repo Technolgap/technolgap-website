2d5f42684b3239938a636476540509f0960cbc61
Jothika Sundaram <jsund301@gmail.com>
2020-08-24 17:44:02 -0400

    create home and about elements, add page navigation between home and about pages

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,5 +1,25 @@
 import styles from "./NavBar.module.scss";
+import Link from "next/link";
 
-const NavBar = () => <div className={styles.NavBar}>NAVBAR</div>;
+const NavBar = () => 
+<div className={styles.NavBar}>
+
+  <div className = {styles.technolgap}>technolgap</div>
+
+  <div className = {styles.navButtons}>
+      <Link href = {`\\`}>
+            <h4>home</h4>
+      </Link>
+      
+  </div>
+  
+   <div className = {styles.navButtons}>
+        <Link href = "/about">
+             <h4>about</h4>
+        </Link>
+    
+    </div> 
+   
+</div>;
 
 export default NavBar;
