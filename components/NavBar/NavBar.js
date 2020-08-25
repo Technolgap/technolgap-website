36481cd34e75f6be9cfc5a988380f8a94f028ffc
Jothika Sundaram <jsund301@gmail.com>
2020-08-25 19:07:40 -0400

    add active page hover effect over home and about navigation links

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -7,19 +7,23 @@ const NavBar = () =>
   <div className = {styles.technolgap}>technolgap</div>
 
   <div className = {styles.navButtons}>
+      <div className = {styles.activePage}></div>  
+
       <Link href = {`\\`}>
             <h4>home</h4>
       </Link>
       
   </div>
   
-   <div className = {styles.navButtons}>
+   <div id = 'about' className = {styles.navButtons}>
+         <div className = {styles.activePage}></div>
+
         <Link href = "/about">
              <h4>about</h4>
         </Link>
     
     </div> 
-   
+  
 </div>;
 
 export default NavBar;
