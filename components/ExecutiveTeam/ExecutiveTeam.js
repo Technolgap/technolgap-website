13ee99f776f885d9c9b94c614ee55c45c1ec7074
Monica Vu <vu.monica.05@gmail.com>
2020-08-25 20:39:58 -0400

    testing executive team gallery

--- components/ExecutiveTeam/ExecutiveTeam.js
+++ components/ExecutiveTeam/ExecutiveTeam.js
@@ -1,5 +1,28 @@
 import styles from  "./ExecutiveTeam.module.scss";
+import Title from "../Title/Title"
 
-const ExecutiveTeam = () => <div className={styles.ExecutiveTeam}> Executive Team Gallery </div>;
+const AlexaIcon = require("../../images/Executives/AlexaIcon.svg");
+
+const ExecutiveMember = ({ icon, name, title }) => (
+    <div clasName={styles.Member}>
+        <img src={icon} className={styles.responsive} />
+        <p>{name}</p>
+        <p>{title}</p>
+    </div>
+);
+
+const Members = [
+    <ExecutiveMember icon={AlexaIcon} name="Alexa Liaskovski" title="co-founder" />,
+    <ExecutiveMember icon={AlexaIcon} name="Alexa Liaskovski" title="co-founder" />,
+]
+
+const ExecutiveTeam = () => 
+
+<div className={styles.ExecutiveTeam}> 
+    <Title> executive team </Title>
+    <div className={styles.ExecutiveMembers}>
+        {Members}
+    </div>
+</div>
 
 export default ExecutiveTeam;
