2bc968591b30fe71a07ad33cdddb29ec732dbf14
Monica Vu <vu.monica.05@gmail.com>
2020-08-27 15:32:18 -0400

    added all executives and titles

--- components/ExecutiveTeam/ExecutiveTeam.js
+++ components/ExecutiveTeam/ExecutiveTeam.js
@@ -2,18 +2,25 @@ import styles from  "./ExecutiveTeam.module.scss";
 import Title from "../Title/Title"
 
 const AlexaIcon = require("../../images/Executives/AlexaIcon.svg");
+const GinaIcon = require("../../images/Executives/GinaIcon.svg");
+const KateIcon = require("../../images/Executives/KateIcon.svg");
+const MargieIcon = require("../../images/Executives/MargieIcon.svg");
+const MonicaIcon = require("../../images/Executives/MonicaIcon.svg");
 
 const ExecutiveMember = ({ icon, name, title }) => (
     <div clasName={styles.Member}>
         <img src={icon} className={styles.responsive} />
-        <p>{name}</p>
+        <h2> {name} </h2>
         <p>{title}</p>
     </div>
 );
 
 const Members = [
     <ExecutiveMember icon={AlexaIcon} name="Alexa Liaskovski" title="co-founder" />,
-    <ExecutiveMember icon={AlexaIcon} name="Alexa Liaskovski" title="co-founder" />,
+    <ExecutiveMember icon={GinaIcon} name="Gina Bak" title="co-founder" />,
+    <ExecutiveMember icon={MargieIcon} name="Margie Venes" title="director of external communications" />,
+    <ExecutiveMember icon={MonicaIcon} name="Monica Vu" title="director of development" />,
+    <ExecutiveMember icon={KateIcon} name="Kate Allsebrook" title="director of engagement" />,
 ]
 
 const ExecutiveTeam = () => 
