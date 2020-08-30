4a3828084bc2ff89dab26c36d48b6684389d085f
Monica Vu <vu.monica.05@gmail.com>
2020-08-30 12:03:44 -0400

    need new icons

--- components/ExecutiveTeam/ExecutiveTeam.js
+++ components/ExecutiveTeam/ExecutiveTeam.js
@@ -7,23 +7,24 @@ const KateIcon = require("../../images/Executives/KateIcon.svg");
 const MargieIcon = require("../../images/Executives/MargieIcon.svg");
 const MonicaIcon = require("../../images/Executives/MonicaIcon.svg");
 
-const ExecutiveMember = ({ icon, name, title }) => (
+const ExecutiveMember = ({ icon, firstname, lastname, title }) => (
     <div className={styles.Member}>
         <img src={icon} className={styles.responsive} />
-        <h2> {name} </h2>
+        <h2> {firstname} </h2>
+        <h2> {lastname} </h2>
         <p>{title}</p>
     </div>
 );
 
 const Members = [
-    <ExecutiveMember icon={AlexaIcon} name="Alexa Liaskovski" title="co-founder" />,
-    <ExecutiveMember icon={GinaIcon} name="Gina Bak" title="co-founder" />,
-    <ExecutiveMember icon={MargieIcon} name="Margie Venes" title="director of external communications" />,
-    <ExecutiveMember icon={MonicaIcon} name="Monica Vu" title="director of development" />,
-    <ExecutiveMember icon={KateIcon} name="Kate Allsebrook" title="director of engagement" />,
-    <ExecutiveMember icon={KateIcon} name="Mukta Manhas "title="director of mentorship" />,
-    <ExecutiveMember icon={KateIcon} name="Nishtha Mavai" title="director of internal culture" />,
-    <ExecutiveMember icon={KateIcon} name="Joyce Bacic" title="director of mental health and wellness" />,
+    <ExecutiveMember icon={AlexaIcon} firstname="Alexa" lastname="Liaskovski" title="co-founder" />,
+    <ExecutiveMember icon={GinaIcon} firstname="Gina" lastname="Bak" title="co-founder" />,
+    <ExecutiveMember icon={MargieIcon} firstname="Margie" lastname="Venes" title="director of external communications" />,
+    <ExecutiveMember icon={MonicaIcon} firstname="Monica" lastname="Vu" title="director of development" />,
+    <ExecutiveMember icon={KateIcon} firstname="Kate" lastname="Allsebrook" title="director of engagement" />,
+    <ExecutiveMember icon={KateIcon} firstname="Mukta" lastname="Manhas" title="director of mentorship" />,
+    <ExecutiveMember icon={KateIcon} firstname="Nishtha" lastname="Mavai" title="director of internal culture" />,
+    <ExecutiveMember icon={KateIcon} firstname="Joyce" lastname="Bacic" title="director of mental health and wellness" />,
 ]
 
 const ExecutiveTeam = () => 
