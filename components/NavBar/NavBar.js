9a8a1a244eecc43c57c809508d4b9ce41849eb04
Jothika Sundaram <jsund301@gmail.com>
2020-08-31 20:29:37 -0400

    add technol-leaf logo and made active page dynamic sizing

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,24 +1,26 @@
 import styles from "./NavBar.module.scss";
 import ActiveLink from "./ActiveLink";
+const leafLogo =  require("../../images/NavBar/leafLogo.svg");
 
 
 const NavBar=()=>
   
-
-   <div  className={styles.NavBar}>
-
-    <div className = {styles.technolgap}>technolgap</div>
+   
+    <div  className={styles.NavBar}>
+   
+        <div className = {styles.technolgap}> 
+        <img src = {leafLogo} className = {styles.logo}></img>
+        technolgap
+        </div>
   
-
         <ActiveLink href = {`/`}>
-                <h4>home</h4>
+            <h4>home</h4>
         </ActiveLink >
-        
 
         <ActiveLink  href = "/about">
             <h4>about</h4>
         </ActiveLink >
         
-  </div>;
+    </div>;
 
 export default NavBar;
