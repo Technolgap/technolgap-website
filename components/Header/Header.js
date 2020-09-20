d5d1f36241263a4b9c66c26e01e51c1705e5d5c1
Safa <safa.howaid@gmail.com>
2020-09-20 19:04:15 -0400

    Update header format & add responsive sizing

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,7 +1,5 @@
 import styles from "./Header.module.scss";
 
-const Header = () => <div className={styles.HeaderMargin}>
-                       <div className={styles.Header}></div>
-                     </div>
+const Header = () => <div className={styles.Header}></div>
 
 export default Header;
