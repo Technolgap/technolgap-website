f1f8e51160eed3b5925d04617f8ab90d5fc952d6
mukta-m <mukta.manhas26@gmail.com>
2020-09-26 01:40:18 -0400

    questions page

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -17,34 +17,34 @@ const Form = () =>
 
         <select name="gender" id="gender" required>
             <option value="gender" selected disabled>gender*</option>
-            <option value="woman">woman</option>
-            <option value="man">man</option>
-            <option value="non-binary">non-binary</option>
-            <option value="other">other</option>
+            <option value="Woman">woman</option>
+            <option value="Man">man</option>
+            <option value="Non-Binary">non-binary</option>
+            <option value="Other">other</option>
         </select>
 
         <input type="text" name="other-gender" id="other-gender" placeholder="  if other gender, please specify (put N/A otherwise)*" required></input>
 
         <select name="school" id="school" required>
             <option value="school" selected disabled>school*</option>
-            <option value="carleton">carleton university</option>
-            <option value="uottawa">university of ottawa</option>
-            <option value="algonquin">algonquin college</option>
-            <option value="other">other</option>
+            <option value="Carleton University">carleton university</option>
+            <option value="University of Ottawa">university of ottawa</option>
+            <option value="Algonquin College">algonquin college</option>
+            <option value="Other">other</option>
         </select>
 
         <input type="text" name="other-school" id="other-school" placeholder="  if other school, please specify (put N/A otherwise)*" required></input>
 
 
-        <input type="text" name="name" id="program" placeholder="program*" required></input>
+        <input type="text" name="program" id="program" placeholder="program*" required></input>
 
-        <input type="text" name="name" id="program" placeholder="referral"></input> <br />
+        <input type="text" name="referral" id="referral" placeholder="referral"></input> <br />
 
         <label>what are you hoping to get out of joining Technolgap?*</label><br />
-        <textarea name="message" className={styles.textMessage} id="message" placeholder="  " required></textarea>< br />
+        <textarea name="Why Technolgap?" className={styles.textMessage} id="Why Technolgap?" placeholder="  " required></textarea>< br />
 
         <label for="message">what is the most exciting thing about software development for you?*</label><br />
-        <textarea name="message" className={styles.textMessage} id="message" placeholder="  " required></textarea><br />
+        <textarea name="Software Development" className={styles.textMessage} id="Software Development" placeholder="  " required></textarea><br />
 
         <button type="submit" value="submit">sign up</button>
     
---
+++ pages/question.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout/Layout";
+import Quote from "../components/Quote/Quote"
+import Form from "../components/Form/Form"
+import Questions from "../components/Form/Questions"
+
+const question = () => <Layout>
+    <Questions />
+    
+    </Layout>;
+
+export default question;
