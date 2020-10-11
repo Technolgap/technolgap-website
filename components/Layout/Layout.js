b7d38504e60b9026783208dcfad33a9b1f53e67c
Monica Vu <vu.monica.05@gmail.com>
2020-10-10 20:08:47 -0400

    updated to google calendar

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,3 @@
-import Head from 'next/head';
-// Other imports
-
 import styles from './Layout.module.scss';
 import NavBar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
@@ -10,13 +7,6 @@ import Footer from '../Footer/Footer';
 const Layout = ({ children }) => {
   return (
     <>
-      <Head>
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://uicdn.toast.com/tui-calendar/latest/tui-calendar.css"
-      />
-      </Head>
       <NavBar />
       <div className={styles.Layout}>{children}</div>
       <Footer />
