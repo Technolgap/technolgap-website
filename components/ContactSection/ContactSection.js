445967e645c6b81b040d8f14870dcf6c1bfbd7be
Alisa Ros <60326069+awildecat@users.noreply.github.com>
2020-11-09 21:47:56 -0500

    What is Technolgap & Questions Sections (#17)
    * made the sections
    
    * fixed bugs

--- components/ContactSection/ContactSection.js
+++ components/ContactSection/ContactSection.js
@@ -1,5 +1,13 @@
 import styles from "./ContactSection.module.scss";
 
-const ContactSection = () => <div className={styles.ContactSection}> Have a question? Email us </div>;
+const ContactSection = () => <div className={styles.ContactSection}> 
+    <div className={styles.jumbotron}>
+        <h1 className={styles.HQTitle}>have questions?</h1>
+        <p className={styles.HQText}><strong>Email us!</strong> We’ll do our best to get back to you within four business days.</p>
+        <div className={styles.centering}>
+            <button type="button" href="#" className={styles.HQButton}>email us</button>
+        </div>
+    </div>
+</div>;
 
 export default ContactSection;
