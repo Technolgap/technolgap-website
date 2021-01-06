0f10292dd69e968c2881dde4d309362475986906
Monica Vu <monicavu@monicas-mbp.lan>
2021-01-05 20:34:50 -0500

    fixed links

--- components/ContactSection/ContactSection.js
+++ components/ContactSection/ContactSection.js
@@ -1,11 +1,12 @@
 import styles from "./ContactSection.module.scss";
+import Link from 'next/link';
 
 const ContactSection = () => <div className={styles.ContactSection}> 
     <div className={styles.jumbotron}>
         <h1 className={styles.HQTitle}>have questions?</h1>
         <p className={styles.HQText}><strong>Email us!</strong> We’ll do our best to get back to you within four business days.</p>
         <div className={styles.centering}>
-            <button type="button" href="#" className={styles.HQButton}>email us</button>
+            <Link href="/questions"><button type="button" className={styles.HQButton}>email us</button></Link> 
         </div>
     </div>
 </div>;
