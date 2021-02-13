93eff958d60ffd599b1472903584b333ac88c8b7
eriicali <ericali3@cmail.carleton.ca>
2021-02-13 01:55:39 -0500

    Fixed type in upcomingEvents

--- components/UpcomingEvents/UpcomingEvents.js
+++ components/UpcomingEvents/UpcomingEvents.js
@@ -3,7 +3,7 @@ import styles from "./UpcomingEvents.module.scss";
 const UpcomingEvents = () => 
 <div className={styles.backgroundSection}> 
     <div className={styles.title}>
-        <h1 className={styles.UpcomingEvents}>upcoming events</h1>
+        <h1 className={styles.upcomingEvents}>upcoming events</h1>
         <p className={styles.paragraph}><strong>Technolgap</strong> is dedicated to sharing knowledge and information. Check out our upcoming events, all lead and organized by women in tech! We have three main categories of events; professional development, technical, and health and wellness.</p>
     </div>
 </div>;
