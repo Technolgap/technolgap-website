681f7732ac1a76c22daf31f1a6c2c6761014b61a
eriicali <ericali3@cmail.carleton.ca>
2021-03-12 21:13:13 -0500

    fixed styling

--- components/UpcomingEvents/UpcomingEvents.js
+++ components/UpcomingEvents/UpcomingEvents.js
@@ -5,7 +5,7 @@ const UpcomingEvents = () =>
     <div className={styles.titleBackground}>
         <h1 className={styles.title}>upcoming events</h1>
     </div>
-    <p className={styles.paragraph}><strong>Technolgap</strong> is dedicated to sharing knowledge and information. Check out our upcoming events, all lead and organized by women in tech! We have three main categories of events; professional development, technical, and health and wellness.</p>
+    <p className={styles.paragraph}><strong>Technolgap </strong>  is dedicated to sharing knowledge and information. Check out our upcoming events, all lead and organized by women in tech! We have three main categories of events; professional development, technical, and health and wellness.</p>
 </div>;
 
 export default UpcomingEvents;
