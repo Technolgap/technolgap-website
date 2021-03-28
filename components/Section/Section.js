15684322ec3d22c43f15e3c789c9c09e4eb6fdd9
Maya <mayajwong8@gmail.com>
2021-03-28 18:47:53 -0400

    Fixed hover effect on buttons

--- components/Section/Section.js
+++ components/Section/Section.js
@@ -1,15 +1,23 @@
 import styles from "./Section.module.scss";
 import Link from 'next/link';
+import { useState } from "react";
 
 function Section(props){
     const {title, body, buttons, color} = props;
+    const [onHover, setOnHover] = useState(false);
+    const [hoveredButton, setHoveredButton] = useState(-1);
+
     return(
-        <div className={styles.Section} style={{ backgroundColor: color }}>
+        <div className={styles.Section} style={{backgroundColor: color}}>
             <div className={styles.jumbotron}>
                 <h1 className={styles.HQTitle}>{title}</h1>
                 <p className={styles.HQText}>{body}</p>
                 <div className={styles.centering}>
-                    {buttons.map((button_text) => <Link href="/about"><button type="button" style={{ backgroundColor: color }} className={styles.button}>{button_text}</button></Link>)}
+                    {buttons.map((button_text, index) => <Link href="/about">
+                        <button type="button" style={{ backgroundColor: hoveredButton == index ? 'white' : `${color}`, color: hoveredButton == index ? `${color}`: 'white'}} 
+                        onMouseEnter= {()=> {setOnHover(true); setHoveredButton(index)}}
+                        onMouseLeave= {() => {setOnHover(false); setHoveredButton(-1)}}
+                     className={styles.button}>{button_text}</button></Link>)}
                 </div>
             </div>
         </div>
